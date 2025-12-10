5a1e72a8eef9b14d1d98a436bfef1335599ae053
ilikkako <kotajarvi.ilkka@gmail.com>
2025-12-10 16:50:18 -0600

    Refactor code. Add CalculatorEngine class

--- calculator.py
+++ calculator.py
@@ -6,12 +6,10 @@ gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
 from gi.repository import Gtk, Adw, Gdk
 
-numbers = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
-operators = ('+', '-', 'x', '/')
-others = ('=', 'C', '(', ')', '<<<', '.')
-buttons = numbers + operators + others
-screen = "" # Stores values in calculator screen
-light_mode = True
+# numbers = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+# operators = ('+', '-', 'x', '/')
+# others = ('=', 'C', '(', ')', '<<<', '.')
+# buttons = numbers + operators + others
 
 
 @Gtk.Template(filename="calculator.ui")
@@ -22,76 +20,64 @@ class MainWindow(Gtk.ApplicationWindow):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.calculator = CalculatorEngine()
+        self.light_mode = True
     
 
     def update_display(self, content):
         self.display.set_text(content)
     
 
-    def solve(self, expression):
-        try:
-            new_expression = expression.replace('x', '*') # Fix multiply symbol so eval function can solve the problem 
-            solution = eval(new_expression)
-            solution = round(solution, 6) # Round to avoid rounding error
-        except Exception as e:
-            print(e)
-            return 'ERROR!'
-
-        return str(solution)
-    
-
     @Gtk.Template.Callback()
-    def switch_mode(self, button):
+    def switch_light_mode(self, button):
         app = self.get_application()
         sm = app.get_style_manager()
-        global light_mode
-        if light_mode:
+        if self.light_mode:
             sm.set_color_scheme(Adw.ColorScheme.PREFER_DARK)
-            light_mode = False
+            self.light_mode = False
         else:
             sm.set_color_scheme(Adw.ColorScheme.PREFER_LIGHT)
-            light_mode = True
+            self.light_mode = True
 
     @Gtk.Template.Callback()
     def on_button_clicked(self, button):
         input = button.get_label()
 
-        global screen
-        if screen == 'ERROR!':
-            screen = ''
+        if self.calculator.get_expression() == 'ERROR!':
+            self.calculator.clear()
 
-        # Change operator if there is no number
-        try:
-            if screen[-1] in operators and input in operators:
-                screen = screen[:-1]
-        except:
-            pass
+        # # Change operator if there is no number
+        # try:
+        #     if screen[-1] in operators and input in operators:
+        #         screen = screen[:-1]
+        # except:
+        #     pass
 
-        # Remove first operator if it is not minus or paranthesis
-        try:
-            if screen[0] not in str(numbers):
-                if screen[0] == '-':
-                    pass
-                elif screen[0] == '(':
-                    pass
-                else:
-                    screen = screen[1:] 
-        except:
-            pass
+        # # Remove first operator if it is not minus or paranthesis
+        # try:
+        #     if screen[0] not in str(numbers):
+        #         if screen[0] == '-':
+        #             pass
+        #         elif screen[0] == '(':
+        #             pass
+        #         else:
+        #             screen = screen[1:] 
+        # except:
+        #     pass
 
         if input == '=': # Solve the expression
-            screen = self.solve(screen)
-        elif input == 'C': # Erase the whole expression
-            screen = ''
+            self.calculator.calculate()
+        elif input == 'C': # Clear the whole expression
+            self.calculator.clear()
         elif input == '<<': # Erase a character
-            screen = screen[:-1]
+            self.calculator.erase()
         else: # Add number or operator to the expression
-            screen = screen + str(input)
+            self.calculator.add_input(input)
         
-        self.update_display(screen)
+        self.update_display(self.calculator.get_expression())
 
 
-class AppCalculator(Adw.Application):
+class Application(Adw.Application):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.connect('activate', self.on_activate)
@@ -108,5 +94,37 @@ class AppCalculator(Adw.Application):
         Gtk.StyleContext.add_provider_for_display(display, css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
 
 
-app = AppCalculator(application_id="com.github.ilkkako.gtk4-python-calculator")
+class CalculatorEngine():
+    def __init__(self):
+        self.expression = ''
+
+
+    def add_input(self, value):
+        self.expression += value
+
+
+    def clear(self):
+        self.expression = ''
+    
+
+    def erase(self): 
+        self.expression = self.expression[:-1]
+
+
+    def calculate(self):
+        try:
+            new_expression = self.expression.replace('x', '*') # Fix multiply symbol so eval function can solve the problem 
+            solution = eval(new_expression)
+            solution = round(solution, 6) # Round to avoid rounding error
+            self.expression = str(solution)
+        except Exception as e:
+            print(e)
+            self.expression = 'ERROR!'
+
+
+    def get_expression(self):
+        return self.expression
+
+
+app = Application(application_id="com.github.ilkkako.gtk4-python-calculator")
 app.run(sys.argv)
